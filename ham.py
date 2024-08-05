c3234cd3efc5645ccf237b12985f0fa4b9801611
Marco Bornstein <marcob@umd.edi>
2024-08-05 01:41:17 -0400

    bug fix

--- ham.py
+++ ham.py
@@ -232,8 +232,9 @@ if __name__ == '__main__':
     else:
         batches, remainder = divmod(num_data, batch_size)
         steps_per_epoch = batches if remainder == 0 else batches + 1
-        nonuniform_federated_training(model, FLC, train_loader, test_loader, device, loss_fn, optimizer,
-                                      steps_per_epoch, num_epochs, log_frequency, recorder, None, local_steps=6)
+        loss_fed = nonuniform_federated_training(model, FLC, train_loader, test_loader, device, loss_fn, optimizer,
+                                                 steps_per_epoch, num_epochs, log_frequency, recorder, None,
+                                                 local_steps=6)
 
     MPI.COMM_WORLD.Barrier()
 
